037c6eb3d6d89fa3da14f889ee1dccb5c3d30940
disdong123 <gksrlfw@gmail.com>
2023-09-02 17:34:53 +0900

    @transactional 테스트

--- srt-jpa/build.gradle.kts
+++ srt-jpa/build.gradle.kts
@@ -1,5 +1,16 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    alias(libs.plugins.kotlin.plugin.jpa)
+}
+
 dependencies {
-    implementation(libs.spring.boot.starter.web)
+    api(libs.spring.boot.starter.data.jpa)
+    api(libs.hibernate.types)
+    api(libs.infobip.spring.data.jpa.querydsl.boot.starter)
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    runtimeOnly(libs.mysql.connector.java)
+    // https://mvnrepository.com/artifact/com.h2database/h2
+    testImplementation("com.h2database:h2:2.2.220")
 }
 
 tasks.getByName("bootJar") {
--- srt-jpa/src/test/kotlin/kr/disdong/spring/reflection/test/srt/jpa/JpaApplicationKtTest.kt
+++
@@ -1,7 +0,0 @@
-package kr.disdong.spring.reflection.test.srt.jpa
-
-import org.junit.jupiter.api.Assertions.*
-
-internal class JpaApplicationKtTest {
-
-}
